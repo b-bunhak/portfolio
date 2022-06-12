2b629b89ba38d3dbdf6a89bdaa837c6a9d4ab10a
Bruno Bunhak <b.bunhak@gmail.com>
2022-06-12 01:59:55 -0300

    Mui CssBaseline and Theme

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+
+const theme = createTheme();
+
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</>
+	);
 }
 
 export default MyApp;
